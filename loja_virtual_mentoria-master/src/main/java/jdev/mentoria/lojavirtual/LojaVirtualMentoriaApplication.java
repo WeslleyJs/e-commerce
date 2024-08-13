48f5a895c98dc70e3e0e47b6fcdbd1688afb1194
WeslleyJs <weslley2106@gmail.com>
2024-08-12 21:28:47 -0300

    Relações JPA

---
+++ loja_virtual_mentoria-master/src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
+	}
+}
---
+++ loja_virtual_mentoria-master/src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
@@ -0,0 +1,136 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jdev.mentoria.lojavirtual.enums.TipoEndereco;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name =  "endereco")
+@SequenceGenerator(name = "seq_endereco", sequenceName = "seq_endereco", allocationSize = 1, initialValue = 1)
+public class Endereco implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_endereco")
+    private Long id;
+
+    private String ruaLogra;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String uf;
+    private String cidade;
+    private String pessoaFk;
+
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuaLogra() {
+        return ruaLogra;
+    }
+
+    public void setRuaLogra(String ruaLogra) {
+        this.ruaLogra = ruaLogra;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPessoaFk() {
+        return pessoaFk;
+    }
+
+    public void setPessoaFk(String pessoaFk) {
+        this.pessoaFk = pessoaFk;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return Objects.equals(id, endereco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
