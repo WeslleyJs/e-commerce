b836e96526714faaaa4f83499173c02742fd6d94
WeslleyJs <weslley2106@gmail.com>
2024-09-15 00:25:20 -0300

    Feat: Novos models

---
+++ loja_virtual_mentoria-master/src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,26 @@
+package jdev.mentoria.lojavirtual.enums;
+
+public enum StatusContaPagar {
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada"),
+    ALUGUEL("Aluguel"),
+    FUNCIONARIO("Funcionário"),
+    NEGOCIADA("Renegociada");
+
+
+    private String descricao;
+
+    private StatusContaPagar(String descricao){
+        this.descricao = descricao;
+    }
+    private String getDescricao(){
+        return  descricao;
+    }
+    @Override
+    public String toString(){
+        return this.descricao;
+    }
+
+}
